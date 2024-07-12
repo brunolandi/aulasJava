0a077735fbc154bf1c6d96e075a030f8cd08ad75
Bruno <brulandi@hotmail.com>
2024-07-11 23:25:50 -0300

    feat: novos exercicios

---
+++ curso_programacao/src/repeticoes/exercicioFor2.java
@@ -0,0 +1,21 @@
+package repeticoes;
+
+public class exercicioFor2 {
+
+	public static void main(String[] args) {
+
+		// contagem
+		for (int i = 0; i <= 5; i++) {
+			System.out.println("Valor de i: " + i);
+		}
+
+		System.out.println();
+
+		// contagem regressiva
+		for (int i = 5; i >= 0; i--) {
+			System.out.println("Valor de i: " + i);
+		}
+
+	}
+
+}
---
+++ curso_programacao/src/repeticoes/testeMesaFor.java
@@ -0,0 +1,19 @@
+package repeticoes;
+
+public class testeMesaFor {
+
+	public static void main(String[] args) {
+
+		int x = 3;
+		int y = 0;
+
+		for (int i = 0; i < x; i++) {
+			System.out.print(i + ",");
+			y = y + 5;
+			System.out.println(y);
+
+		}
+
+	}
+
+}
